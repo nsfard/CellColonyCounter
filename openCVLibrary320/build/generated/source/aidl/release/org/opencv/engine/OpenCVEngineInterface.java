e53c72ca7fd8a31597d7060fd4c8c9cdd9965dc7
ak2795 <andrew.kym7@gmail.com>
2017-02-05 18:26:39 -0800

    After initial build and run
    This is the update without changing any code and just running the app on
    the phone.

--- openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/nsfard/CellColonyCounter/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\andre\\Documents\\GitHub\\CellColonyCounter\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
